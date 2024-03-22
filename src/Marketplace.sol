33b4b3162228e384b667088af72befb87324323e
Van Hohenheim <0xhohenheim@gmail.com>
2024-03-23 03:15:48 +0530

    fix: marketplace formatting

--- src/Marketplace.sol
+++ src/Marketplace.sol
@@ -1,12 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-
 import "./ERC721.sol";
 import "./ERC1155.sol";
- import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
-
-
+import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 
 contract WebtoonSale {
     ERC721Webtoon public immutable erc721Contract;
@@ -20,14 +17,22 @@ contract WebtoonSale {
 
     mapping(uint256 => Listing) public listings;
 
-    uint256 public marketplaceFee; 
+    uint256 public marketplaceFee;
     address public feeRecipient;
 
-    event Listed(uint256 indexed tokenId, address indexed seller, uint256 price);
-    event Sold(uint256 indexed tokenId, address indexed seller, address indexed buyer, uint256 price); 
-    event DeListed(uint256 indexed tokenId); 
+    event Listed(
+        uint256 indexed tokenId,
+        address indexed seller,
+        uint256 price
+    );
+    event Sold(
+        uint256 indexed tokenId,
+        address indexed seller,
+        address indexed buyer,
+        uint256 price
+    );
+    event DeListed(uint256 indexed tokenId);
     event PriceUpdated(uint256 indexed tokenId, uint256 newPrice);
-
 
     error InsufficientFunds();
     error NFTnotListed();
@@ -41,7 +46,12 @@ contract WebtoonSale {
      * @param _marketplaceFee The marketplace fee (in basis points, e.g., 250 = 2.5%).
      * @param _feeRecipient The address to receive marketplace fees.
      */
-    constructor(address _erc721Address, address _erc1155Address, uint256 _marketplaceFee, address _feeRecipient) {
+    constructor(
+        address _erc721Address,
+        address _erc1155Address,
+        uint256 _marketplaceFee,
+        address _feeRecipient
+    ) {
         erc721Contract = ERC721Webtoon(_erc721Address);
         erc1155Contract = ERC1155WebtoonHolder(_erc1155Address);
         marketplaceFee = _marketplaceFee;
@@ -58,7 +68,7 @@ contract WebtoonSale {
             revert NotOwner();
         }
 
-        if (erc721Contract.getApproved(tokenId) != address(this)) { 
+        if (erc721Contract.getApproved(tokenId) != address(this)) {
             revert NotApproved();
         }
 
@@ -82,49 +92,47 @@ contract WebtoonSale {
         }
 
         // Calculate marketplace fee
-       uint256 feeAmount = (listing.price * marketplaceFee) / 10000; 
-       uint256 sellerPayout = listing.price - feeAmount;
+        uint256 feeAmount = (listing.price * marketplaceFee) / 10000;
+        uint256 sellerPayout = listing.price - feeAmount;
 
-       (bool feeSuccess, ) = payable(feeRecipient).call{value: feeAmount}("");
-       (bool sellerSuccess, ) = payable(listing.seller).call{value: sellerPayout}(""); 
-       require(feeSuccess && sellerSuccess, "Payment transfers failed");
-       erc1155Contract.mintFromERC721(tokenId, buyer);
+        (bool feeSuccess, ) = payable(feeRecipient).call{value: feeAmount}("");
+        (bool sellerSuccess, ) = payable(listing.seller).call{
+            value: sellerPayout
+        }("");
+        require(feeSuccess && sellerSuccess, "Payment transfers failed");
+        erc1155Contract.mintFromERC721(tokenId, buyer);
 
-       emit Sold(tokenId, listing.seller, buyer, listing.price);
+        emit Sold(tokenId, listing.seller, buyer, listing.price);
     }
 
     /**
      * @dev Allows the seller to de-list a previously listed ERC721 token.
      * @param tokenId The ID of the ERC721 token to de-list.
-    */
+     */
     function delistNFT(uint256 tokenId) public {
-    Listing memory listing = listings[tokenId];
+        Listing memory listing = listings[tokenId];
 
-    if (listing.seller != msg.sender) {
-        revert NotOwner();
-    }
+        if (listing.seller != msg.sender) {
+            revert NotOwner();
+        }
 
-    delete listings[tokenId]; 
-    emit DeListed(tokenId);
-    
+        delete listings[tokenId];
+        emit DeListed(tokenId);
     }
 
     /**
-    * @dev Allows the seller to update the price of a listed ERC721 token.
-    * @param tokenId The ID of the ERC721 token to update the price for.
-    * @param newPrice The new sale price.
-    */
+     * @dev Allows the seller to update the price of a listed ERC721 token.
+     * @param tokenId The ID of the ERC721 token to update the price for.
+     * @param newPrice The new sale price.
+     */
     function updatePrice(uint256 tokenId, uint256 newPrice) public {
-    Listing storage listing = listings[tokenId]; 
+        Listing storage listing = listings[tokenId];
 
-    if (listing.seller != msg.sender) {
-        revert NotOwner();
+        if (listing.seller != msg.sender) {
+            revert NotOwner();
+        }
+
+        listing.price = newPrice;
+        emit PriceUpdated(tokenId, newPrice);
     }
-
-    listing.price = newPrice;
-    emit PriceUpdated(tokenId, newPrice); 
-    
-    }
-
-
 }
