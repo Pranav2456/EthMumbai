07ac64f3fb3c823e5f79edf5e01ee9cf3e3cb535
Van Hohenheim <0xhohenheim@gmail.com>
2024-03-23 02:38:36 +0530

    fix: erc721 formatting

--- src/ERC721.sol
+++ src/ERC721.sol
@@ -4,47 +4,48 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-
 error URIAlreadySet();
 error URINotSet();
 
-
 /// @title ERC721 contract for the webtoon NFTs
-contract ERC721Webtoon is ERC721URIStorage, Ownable{
+contract ERC721Webtoon is ERC721URIStorage, Ownable {
     /// @notice The next token ID
     uint256 private _nextTokenId;
     /// @notice Mapping of artist addresses to their approval status
     mapping(address => bool) public isApprovedArtist;
 
-/// @notice Event emitted when a new webtoon is minted
+    /// @notice Event emitted when a new webtoon is minted
     event WebtoonMinted(
         address indexed to,
         uint256 indexed tokenId,
         string tokenURI
     );
-/// @notice Event emitted when an artist is approved
+    /// @notice Event emitted when an artist is approved
     event ArtistApproved(address indexed artistAddress);
 
-   constructor(address initialOwner) ERC721("name","symbol") Ownable(initialOwner){}
+    constructor(
+        address initialOwner
+    ) ERC721("name", "symbol") Ownable(initialOwner) {}
 
-/// @notice Approves an artist to mint webtoons
-   function approveArtist(address artistAddress) public onlyOwner {
+    /// @notice Approves an artist to mint webtoons
+    function approveArtist(address artistAddress) public onlyOwner {
         isApprovedArtist[artistAddress] = true;
         emit ArtistApproved(artistAddress);
     }
 
-/// @notice Mints a new webtoon NFT
-    function mintWebtoon(address to, string memory tokenURI) public returns (uint256){
-        if(!isApprovedArtist[msg.sender]){
+    /// @notice Mints a new webtoon NFT
+    function mintWebtoon(
+        address to,
+        string memory tokenURI
+    ) public returns (uint256) {
+        if (!isApprovedArtist[msg.sender]) {
             revert("Not approved artist");
         } else {
-        uint256 tokenId = _nextTokenId++;
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, tokenURI);
-        emit WebtoonMinted(to, tokenId, tokenURI);
-        return tokenId;
+            uint256 tokenId = _nextTokenId++;
+            _safeMint(to, tokenId);
+            _setTokenURI(tokenId, tokenURI);
+            emit WebtoonMinted(to, tokenId, tokenURI);
+            return tokenId;
         }
     }
-   }
-   
-   
+}
