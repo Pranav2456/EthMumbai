f22dd163c7bb22cfcd578190c998b67dcffa3d53
Van Hohenheim <0xhohenheim@gmail.com>
2024-03-23 06:14:32 +0530

    feat: pausable webtoon minting

--- src/Webtoon.sol
+++ src/Webtoon.sol
@@ -20,8 +20,8 @@ contract Webtoon is ERC721URIStorage, AccessLock {
     function mint(
         address to,
         string memory tokenURI
-    ) public onlyAdmin returns (uint256) {
         uint256 tokenId = _nextTokenId++;
+    ) public onlyAdmin whenNotPaused returns (uint256) {
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, tokenURI);
         emit Minted(to, tokenId, tokenURI);
