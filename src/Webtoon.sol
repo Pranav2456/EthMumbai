f899fa68ed75155551df33c4a4478ffd652c6b1a
Van Hohenheim <0xhohenheim@gmail.com>
2024-03-23 06:14:04 +0530

    chore: webtoon rm unused errors

--- src/Webtoon.sol
+++ src/Webtoon.sol
@@ -4,9 +4,6 @@ pragma solidity 0.8.20;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "./utils/AccessLock.sol";
 
-error URIAlreadySet();
-error URINotSet();
-
 /// @title Webtoon
 /// @author HeimLabs <contact@heimlabs.com>
 /// @notice ERC721 NFT Contract for original Webtoons
