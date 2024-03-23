5aa69dcc7fbadeb13dd5d69fd14086be8368f825
Van Hohenheim <0xhohenheim@gmail.com>
2024-03-23 07:13:02 +0530

    feat: update proxy webtoon interface

--- src/interfaces/IProxyWebtoon.sol
+++ src/interfaces/IProxyWebtoon.sol
@@ -4,5 +4,9 @@ pragma solidity 0.8.20;
 import "@openzeppelin/contracts/token/ERC1155/extensions/IERC1155MetadataURI.sol";
 
 interface IProxyWebtoon is IERC1155MetadataURI {
-    function mint(uint256 webtoonTokenId, address to) external;
+    function mint(
+        address to,
+        uint256 tokenId,
+        string calldata tokenURI
+    ) external;
 }
